600bde4798168e183ccb45183cb37d47d9a5c8a2
Shiva Kumar <shiva.kake@gmail.com>
2023-08-09 10:13:40 +0530

    Initial Commit

---
+++ CallBackClosures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CallBackClosures
+//
+//  Created by Kumaran on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
